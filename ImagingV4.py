ef7c8f17754e584d84613f18ab11283e4cc2da1d
Kevin <->
2014-10-10 17:08:09 +1100

    SelfCal is now able to perfrorm both phase and amplitude selfcal with independant cleaning levels. This cleaning level also depends on how many cycles of selfcal will be done (with the sigma level reducing each time).

--- ImagingV4.py
+++ ImagingV4.py
@@ -19,19 +19,19 @@ if args.TaskSet is not None:
 else:
 	TaskSet = 1 
 
-#get the Imaging Details here. Replace thing with a database call?
+#get the Imaging Details here. Replace this manual mess with a database call?
 
 ImagingDetails = {}
 ProcList = []
 
 #================= Misc =================
 
-ImagingDetails['MaxProcesses'] = 2
+ImagingDetails['MaxProcesses'] = 7
 ImagingDetails['ProjectNum'] = "C2648"
 
-ImagingDetails['FWHM'] = ""
-ImagingDetails['Cell'] = ""
-ImagingDetails['PositionAngle'] = ""
+ImagingDetails['FWHM'] = "25.97,22.13"
+ImagingDetails['Cell'] = "6.96,6.96"
+ImagingDetails['PositionAngle'] = "-1.3"
 
 #================= Locations =================
 
@@ -40,26 +40,41 @@ ImagingDetails['DestinationPath'] = "DestinationTest"
 ImagingDetails['DestinationLink'] = "i"
 
 ImagingDetails['Images'] = ["pnt_1","pnt_2"]
+#ImagingDetails['Images'] = ["pnt_1","pnt_2","pnt_3","pnt_4","pnt_5","pnt_6","pnt_7","pnt_8","pnt_9","pnt_10","pnt_11","pnt_12","pnt_13","pnt_14","pnt_15","pnt_16","pnt_17","pnt_18","pnt_19","pnt_20","pnt_21","pnt_22","pnt_23","pnt_24","pnt_25","pnt_26","pnt_27","pnt_28","pnt_29","pnt_30","pnt_31","pnt_32","pnt_33","pnt_34","pnt_35","pnt_36","pnt_37","pnt_38","pnt_39","pnt_40","pnt_41","pnt_42","pnt_43","pnt_44","pnt_45","pnt_46","pnt_47","pnt_48","pnt_49","pnt_50","pnt_51","pnt_52","pnt_53","pnt_54","pnt_55","pnt_56","pnt_57","pnt_58","pnt_59","pnt_60","pnt_61","pnt_62","pnt_63","pnt_64","pnt_65","pnt_66","pnt_67","pnt_68","pnt_69","pnt_70","pnt_71","pnt_72","pnt_73","pnt_74","pnt_75","pnt_76","pnt_77"]
 
 #================= Invert =================
 
-ImagingDetails['Imsize'] = "4,4,beam"
-ImagingDetails['Offset'] = ""
+ImagingDetails['Imsize'] = "3,3,beam"
+ImagingDetails['Offset'] = "05:17:14.64,-66:58:58.20"
 ImagingDetails['Robust'] = "0"
 ImagingDetails['Frequency'] = "2100"
 ImagingDetails['Stokes'] = "i"
 ImagingDetails['ActiveAntennasName'] = "12345"
 ImagingDetails['ActiveAntennas'] = "\"-ant(6)\""
-ImagingDetails['InvertOptions'] = "double,mfs,sdb"
+ImagingDetails['InvertOptions'] = "double,mfs,sdb,mosaic"
 
 #================= Selfcal =================
+#===== Phase =====
 
-ImagingDetails['SelfCalAmount'] = 1
-ImagingDetails['SelfCalIterations'] = 5500
-ImagingDetails['SelfCalSigma'] = 22
-ImagingDetails['SelfCalOptions'] = "mfs,phase"
-ImagingDetails['SelfCalBin'] = 1
-ImagingDetails['SelfCalInterval'] = 0.1
+ImagingDetails['PhaseSelfCalAmount'] = 2
+ImagingDetails['PhaseSelfCalOptions'] = "mfs,phase"
+
+ImagingDetails['PhaseSelfCalIterations'] = 5500
+ImagingDetails['PhaseSelfCalSigma'] = [50,25]
+
+ImagingDetails['PhaseSelfCalBin'] = 1
+ImagingDetails['PhaseSelfCalInterval'] = 0.1
+
+#=== Amplitude ===
+
+ImagingDetails['AmplitudeSelfCalAmount'] = 1
+ImagingDetails['AmplitudeSelfCalOptions'] = "mfs,amp"
+
+ImagingDetails['AmplitudeSelfCalIterations'] = 5500
+ImagingDetails['AmplitudeSelfCalSigma'] = [15]
+
+ImagingDetails['AmplitudeSelfCalBin'] = 1
+ImagingDetails['AmplitudeSelfCalInterval'] = 0.1
 
 #================= MFClean =================
 
@@ -75,6 +90,8 @@ ImagingDetails['RestorOptions'] = "mfs"
 
 ImagingDetails['Bandwidth'] = 2.049
 
+#================= End Reading Data =================
+
 #Check to see if a particular item/folder exists within a particular folder. default folder to check is current one
 def ReadFolder(ItemToFind, Path=os.getcwd()):
 	for Files in os.listdir(Path):
@@ -88,8 +105,6 @@ def ReadFolder(ItemToFind, Path=os.getcwd()):
 #Check to see how many processes are running at the same time, wait if max limit has been reached. 
 def CheckProc(MaxProcesses):
 	while len(ProcList) > MaxProcesses:
-		print("===================Checking===================")
-
 		time.sleep(3)
 
 		for Proc in ProcList:
@@ -100,21 +115,21 @@ def CheckProc(MaxProcesses):
 def UVaver(Image, ImagingDetails):
 	Out = str(Image) + ".uvaver." + str(ImagingDetails['RoundNum'])
 
-	Task = "Uvaver "
+	Task = "uvaver "
 	Task = Task + " vis='" + str(ImagingDetails['SourcePath']) + "/" + str(Image) + "'"
 	Task = Task + " out='" + ImagingDetails['DestinationLink'] + "/" + str(Out) + "'"
 
 	print Task
 	ProcList.append(Popen(Task, shell=True))
 
-#run the task Invert
+#run the task Invert. This pipes to a LogFile (currently no output to terminal)
 def Invert(Image, ImagingDetails):
 	UVaver = str(Image) + ".uvaver." + str(ImagingDetails['RoundNum'])
 	Map = str(Image) + ".map." + str(ImagingDetails['RoundNum'])
 	Beam = str(Image) + ".beam." + str(ImagingDetails['RoundNum'])
 	LogFile = str(Image) + ".invertlog." + str(ImagingDetails['RoundNum'])
 
-	Task = "Invert "
+	Task = "invert "
 	Task = Task + " vis='" + UVaver + "'"
 	Task = Task + " map='" + Map + "'"
 	Task = Task + " beam='" + Beam + "'"
@@ -131,8 +146,8 @@ def Invert(Image, ImagingDetails):
 	print Task
 	ProcList.append(Popen(Task, shell=True))
 
-#run the task MFClean
-def MFClean(Image, ImagingDetails, SelfCalBool):
+#run the task MFClean. This reads the invert log file and then gets the Theoretical RMS to then times by an amount (i.e 5 sigma) to se the clean cutoff level.
+def MFClean(Image, ImagingDetails, SelfCal):
 	Map = str(Image) + ".map." + str(ImagingDetails['RoundNum'])
 	Beam = str(Image) + ".beam." + str(ImagingDetails['RoundNum'])
 	Model = str(Image) + ".model." + str(ImagingDetails['RoundNum'])
@@ -154,7 +169,7 @@ def MFClean(Image, ImagingDetails, SelfCalBool):
 	Task = Task + " out='" + Model + "'"
 	Task = Task + " region='" + ImagingDetails['CleanRegion'] + "'"
 
-	if SelfCalBool == True:
+	if SelfCal == True:
 		Task = Task + " niters='" + str(ImagingDetails['SelfCalIterations']) + "'"
 		Task = Task + " cutoff='" + str(float(ImagingDetails['SelfCalSigma']) * float(TheoreticalRMS)) + "'"
 	else:
@@ -173,7 +188,6 @@ def SelfCal(Image, ImagingDetails):
 	Task = Task + " vis='"+ UVaver + "'"
 	Task = Task + " model='" + Model + "'"
 	Task = Task + " interval='" + str(ImagingDetails['SelfCalInterval']) + "'"
-	#Task = Task + " select='" + str(InvertAntenna) + "'"
 	Task = Task + " options='" + str(ImagingDetails['SelfCalOptions']) + "'"
 		
 	print Task
@@ -184,7 +198,7 @@ def UVaverSelfCal(Image, ImagingDetails):
 	Vis = str(Image) + ".uvaver." + str(ImagingDetails['RoundNum'])
 	Out = str(Image) + ".uvaver." + str(ImagingDetails['RoundNum'] + 1)
 
-	Task = "Uvaver "
+	Task = "uvaver "
 	Task = Task + " vis='" + Vis + "'"
 	Task = Task + " out='" + Out + "'"
 
@@ -224,10 +238,6 @@ def Linmos(ImagingDetails):
 
 #Standard CABB Pipeline. UVaver, Invert, MFClean, SelfCal (optional - send to start. ), Restor, Linmos
 def StandardCabbImaging(ImagingDetails):
-
-	#if ReadFolder(ImagingDetails['DestinationPath']) == False:
-	#	os.system("mkdir " + ImagingDetails['DestinationPath'])
-
 	ImagingDetails['RoundNum'] = 1
 
 	#===============Run UVaver to apply Calibrators=================
@@ -237,38 +247,60 @@ def StandardCabbImaging(ImagingDetails):
 	CheckProc(0)
 
 	
-	for RoundNum in range(1,ImagingDetails['SelfCalAmount'] + 1):
-		print ("round num = " + str(RoundNum))
+	for RoundNum in range(1,ImagingDetails['PhaseSelfCalAmount'] + ImagingDetails['AmplitudeSelfCalAmount'] + 1):
+		#Assign the options depending on what round we are processing.
+
 		ImagingDetails['RoundNum'] = RoundNum
+
+		# functions (such as mfclean and selfcal will use the values stored below. They will be allied dynamically depedning on what type of self cal is taking place. )
+		if RoundNum <= ImagingDetails['PhaseSelfCalAmount']:
+			ImagingDetails['SelfCalAmount'] = ImagingDetails['PhaseSelfCalAmount']
+			ImagingDetails['SelfCalOptions'] = ImagingDetails['PhaseSelfCalOptions']
+
+			ImagingDetails['SelfCalIterations'] = ImagingDetails['PhaseSelfCalIterations']
+			ImagingDetails['SelfCalSigma'] = ImagingDetails['PhaseSelfCalSigma'][RoundNum - 1]
+
+			ImagingDetails['SelfCalBin'] = ImagingDetails['PhaseSelfCalBin']
+			ImagingDetails['SelfCalInterval'] = ImagingDetails['PhaseSelfCalInterval']
+		else:
+			ImagingDetails['SelfCalAmount'] = ImagingDetails['AmplitudeSelfCalAmount']
+			ImagingDetails['SelfCalOptions'] = ImagingDetails['AmplitudeSelfCalOptions']
+
+			ImagingDetails['SelfCalIterations'] = ImagingDetails['AmplitudeSelfCalIterations']
+			ImagingDetails['SelfCalSigma'] = ImagingDetails['AmplitudeSelfCalSigma'][RoundNum - ImagingDetails['PhaseSelfCalAmount'] - 1]
+
+			ImagingDetails['SelfCalBin'] = ImagingDetails['AmplitudeSelfCalBin']
+			ImagingDetails['SelfCalInterval'] = ImagingDetails['AmplitudeSelfCalInterval']
+
 
 		#===============Run Invert==================
 		for ImageName in ImagingDetails['Images']:
-			ImageName = ImagingDetails['DestinationLink'] + "/" + ImageName
+			ImageName = ImagingDetails['DestinationLink'] + "/" + ImageName + "." + str(ImagingDetails['Frequency'])
 			CheckProc(ImagingDetails['MaxProcesses'])
-			Invert(ImageName + "." + str(ImagingDetails['Frequency']), ImagingDetails);
+			Invert(ImageName, ImagingDetails);
 		CheckProc(0)
 
 
 		#===============Run MFClean==================
 		for ImageName in ImagingDetails['Images']:
-			ImageName = ImagingDetails['DestinationLink'] + "/" + ImageName
+			ImageName = ImagingDetails['DestinationLink'] + "/" + ImageName + "." + str(ImagingDetails['Frequency'])
 			CheckProc(ImagingDetails['MaxProcesses'])
-			MFClean(ImageName + "." + str(ImagingDetails['Frequency']), ImagingDetails, True);
+			MFClean(ImageName, ImagingDetails, True);
 		CheckProc(0)
 
 
 		#===============Run SelfCal==================
 		for ImageName in ImagingDetails['Images']:
-			ImageName = ImagingDetails['DestinationLink'] + "/" + ImageName
+			ImageName = ImagingDetails['DestinationLink'] + "/" + ImageName + "." + str(ImagingDetails['Frequency'])
 			CheckProc(ImagingDetails['MaxProcesses'])
-			SelfCal(ImageName + "." + str(ImagingDetails['Frequency']), ImagingDetails);
+			SelfCal(ImageName, ImagingDetails);
 		CheckProc(0)
 
 		#===============Run UVaver to apply SelfCal==================
 		for ImageName in ImagingDetails['Images']:
-			ImageName = ImagingDetails['DestinationLink'] + "/" + ImageName
+			ImageName = ImagingDetails['DestinationLink'] + "/" + ImageName + "." + str(ImagingDetails['Frequency'])
 			CheckProc(ImagingDetails['MaxProcesses'])
-			UVaverSelfCal(ImageName + "." + str(ImagingDetails['Frequency']), ImagingDetails);
+			UVaverSelfCal(ImageName, ImagingDetails);
 		CheckProc(0)
 
 	#======================================================================================
@@ -280,31 +312,30 @@ def StandardCabbImaging(ImagingDetails):
 
 	#===============Run Invert==================
 	for ImageName in ImagingDetails['Images']:
-		ImageName = ImagingDetails['DestinationLink'] + "/" + ImageName
+		ImageName = ImagingDetails['DestinationLink'] + "/" + ImageName + "." + str(ImagingDetails['Frequency'])
 		CheckProc(ImagingDetails['MaxProcesses'])
-		Invert(ImageName + "." + str(ImagingDetails['Frequency']), ImagingDetails);
+		Invert(ImageName, ImagingDetails);
 	CheckProc(0)
 
 
 	#===============Run MFClean==================
 	for ImageName in ImagingDetails['Images']:
-		ImageName = ImagingDetails['DestinationLink'] + "/" + ImageName
+		ImageName = ImagingDetails['DestinationLink'] + "/" + ImageName + "." + str(ImagingDetails['Frequency'])
 		CheckProc(ImagingDetails['MaxProcesses'])
-		MFClean(ImageName + "." + str(ImagingDetails['Frequency']), ImagingDetails, False);
+		MFClean(ImageName, ImagingDetails, False);
 	CheckProc(0)
 
 	#===============Run Restor==================
 	for ImageName in ImagingDetails['Images']:
-		ImageName = ImagingDetails['DestinationLink'] + "/" + ImageName
+		ImageName = ImagingDetails['DestinationLink'] + "/" + ImageName + "." + str(ImagingDetails['Frequency'])
 		CheckProc(ImagingDetails['MaxProcesses'])
-		Restor(ImageName + "." + str(ImagingDetails['Frequency']), ImagingDetails);
+		Restor(ImageName, ImagingDetails);
 	CheckProc(0)
 
 	#===============Run Linmos==================
-	
 	Linmos(ImagingDetails);
-	#CheckProc(0)
 
+#========================Finish Standard CABB Imaging =====================================
 
 
 if ReadFolder(ImagingDetails['DestinationPath']) == False:
@@ -323,10 +354,10 @@ CheckProc(0);
 
 os.system("rm " + ImagingDetails['DestinationLink'])
 
-print(      "\n\n\n\n+======================Finished======================+\n"       )
-print("|            Time Taken    = " + str(datetime.now()) + "             |")
-print("|            Time Finished = " + str(datetime.now()-startTime) + "             |")
-print(            "\n+====================================================+\n"       )
+print(      "\n\n\n\n+=========================Finished=========================+\n"       )
+print("|        Time Taken    = " + str(datetime.now()) + "        |")
+print("|        Time Finished = " + str(datetime.now()-startTime) + "                    |")
+print(            "\n+==========================================================+\n"       )
 
 
 
